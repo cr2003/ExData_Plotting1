14ac7b9fbebc732a15534f4576ff7a1af58a8400
cr2003 <crodrigo2003@gmail.com>
2014-12-06 12:44:30 +0100

    put png files in folder figure

--- working/plot1.R
+++
@@ -1,8 +0,0 @@
-## plot1.R, creates the histogram with the Global Active Power
-
-plot1 <- function() {
-        hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
-        dev.copy(png, file="plot1.png", width=480, height=480)
-        dev.off()
-}
-plot1()
